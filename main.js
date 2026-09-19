faeae67c23f9df11c487fb5d32b468d5bbfc8b9d
D.M.Kaawya <dmkaawya@gmail.com>
2026-09-19 19:14:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,1 @@
+const navToggle=document.querySelector('.hamb');const links=document.querySelector('.links');if(navToggle&&links){navToggle.addEventListener('click',()=>links.classList.toggle('open'))}
